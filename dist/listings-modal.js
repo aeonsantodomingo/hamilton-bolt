eb27eb4df4ce9309a2343998405a423ea3614a28
aeonsantodomingo <santodomingo.aeon@gmail.com>
2022-07-01 22:09:42 -0700

    added more listings

--- dist/listings-modal.js
+++ dist/listings-modal.js
@@ -1,16 +1,58 @@
-const overlay = document.querySelector("#overlayListing1");
+// Listing 1 
+
+const overlay1 = document.querySelector("#overlayListing1");
+
 
 document.querySelector("#show-modal-btn-1").addEventListener("click", () => {
-  overlay.style.display = "block";
+  overlay1.style.display = "block";
   document.querySelector("html").style.overflow = 'hidden';
   
 })
 document.querySelector("#close-modal-btn-1").addEventListener("click", () => {
-  overlay.style.display = "none";
+  overlay1.style.display = "none";
   document.querySelector("html").style.overflow = 'visible';
 })
+
 document.querySelector("#overlayListing1").addEventListener("click", () => {
-  overlay.style.display = "none";
+  overlay1.style.display = "none";
+  document.querySelector("html").style.overflow = 'visible';
+})
+
+// Listing 2
+
+const overlay2 = document.querySelector("#overlayListing2");
+
+document.querySelector("#show-modal-btn-2").addEventListener("click", () => {
+  overlay2.style.display = "block";
+  document.querySelector("html").style.overflow = 'hidden';
+})
+
+document.querySelector("#close-modal-btn-2").addEventListener("click", () => {
+  overlay2.style.display = "none";
+  document.querySelector("html").style.overflow = 'visible';
+})
+
+document.querySelector("#overlayListing2").addEventListener("click", () => {
+  overlay2.style.display = "none";
+  document.querySelector("html").style.overflow = 'visible';
+})
+
+// Listing 3
+
+const overlay3 = document.querySelector("#overlayListing3");
+
+document.querySelector("#show-modal-btn-3").addEventListener("click", () => {
+  overlay3.style.display = "block";
+  document.querySelector("html").style.overflow = 'hidden';
+})
+
+document.querySelector("#close-modal-btn-3").addEventListener("click", () => {
+  overlay3.style.display = "none";
+  document.querySelector("html").style.overflow = 'visible';
+})
+
+document.querySelector("#overlayListing3").addEventListener("click", () => {
+  overlay3.style.display = "none";
   document.querySelector("html").style.overflow = 'visible';
 })
 
