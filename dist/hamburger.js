a00ba48fd53508e10d353bc8e89a5ee12c88f0c6
aeonsantodomingo <santodomingo.aeon@gmail.com>
2022-07-04 19:47:33 -0700

    finished about & contact page for mobile and desktop

--- dist/hamburger.js
+++ dist/hamburger.js
@@ -6,6 +6,7 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 })
 
+
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
